9fef002c95c7f34ada444b6f0741249d19ccff83
Marcus Mourão <marcusmmourao@gmail.com>
2018-03-26 22:21:06 -0300

    Setup project as REST API

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(200).json({title: "News API - Is Working"})
 });
 
 module.exports = router;

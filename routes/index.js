5f148396b6be756900d194a946a322e5a838c142
Marcus Mourão <marcusmmourao@gmail.com>
2018-03-26 22:47:26 -0300

    Setup eslint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.status(200).json({title: "News API - Is Working"})
+router.get('/', (req, res, next) => {
+  res.status(200).json({title: 'News API - Is Working'});
 });
 
 module.exports = router;
